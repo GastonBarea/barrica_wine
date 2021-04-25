b4f5b6496b393d9ce5f51b7483e46e0e20102d75
GastonBarea <barea_gaston@gmail.com>
2021-04-25 12:09:23 -0300

    Mi primer commit

---
+++ webfrontend/include/header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+   <meta name="keywords" content="barrica, wine, vino, tinto, blanco, varietal, rosado, cabernet sauvignon, malbec, merlot, tempranillo">
+   <meta name="description" content="Barrica Wine donde encontrmos vinos con gran bariedad y calidad para su comercializacion. Eventos de cata de vino y cursos">
+   <meta name="author" content="Anibal Giboudot, Sebastián Martín Perez, Gaston Alan Barea">
+   
+   <link rel="short icon" type="imagen/pmg" href="img/nosotros.png">
+ 
+     <title>BarricaWine</title>
+
+
+    <!--Bootstrap libreria-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!--Google Fonts y Icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <!--mi css-->
+    <link rel="stylesheet" href="css/estilo.css">
+
+</head>
+<body>
+
+    <header>  
+
+        <nav class="navbar navbar-expand-lg fixed-top">
+        
+         <div class="container-fluid barranprincipal">
+         
+          <a href="index.html"><img src="img/bwmin.png" alt="logo" width="59" height="50"></a>
+           
+            <button class="navbar-toggler hamburger hamburger--spin" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="hamburger-box">
+          <span class="hamburger-inner"></span></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+                
+                
+                <ul class="navbar-nav">   
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">
+                            <span class="material-icons">home</span>Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="material-icons">wine_bar</span>Nuestros Vinos</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="productos.php">Linea Tintos</a></li>
+                                    <li><a class="dropdown-item" href="productos.php">Linea Blancos</a></li>
+                                    <li><a class="dropdown-item" href="productos.php">Linea Rosados</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="nosotros.php">
+                                    <span class="material-icons">apartment</span>Nosotros</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="eventos.php">
+                                        <span class="material-icons">event</span>Eventos</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contacto.php">
+                                            <span class="material-icons">contact_page</span>Contacto</a>
+                                        </li>
+<!--usuario y carrito-->
+                                        <ul class="navbar-nav ml-auto">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                <span class="material-icons">account_circle</span></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                <span class="material-icons">shopping_cart</span></a>
+                                            </li>
+                                        </ul>
+                </ul>
+            </div>
+            </div>
+        </nav>
+
+       
+    </header>
+    
+   
---
+++ webfrontend/index.php
@@ -0,0 +1,106 @@
+<?php include_once('include/header.php') /*conecta con archibo header*/?>
+
+
+  <section class="inicio">
+   <div class="container conten-center rounded img-fluid">
+    
+     <h1 class="pres">Barrica</h1>
+     <h2 class="pres">VINOS Y ALGO MÁS</h2>
+     <a href="#" class="btn topmargin-sm"><span class="material-icons">shopping_cart</span></a>
+  
+  </div>
+   
+    
+  </section>
+  
+<!--acá va a ir los destacados-->
+  <section>
+
+  <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <article class="">
+                        <div class="card">
+                        <img src="img/chanar.png" class="card-img-top" alt="vino Historias" width="" height="">
+                        <div class="card-body">
+                            <h3>Card title</h3>
+                            <h4>Chardonay</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                        </div>
+                        </div>
+                    </article>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <article class="">
+                        <div class="card">
+                        <img src="img/historia.png" class="card-img-top" alt="vino Historias" width="" height="">
+                        <div class="card-body">
+                            <h3>Card title</h3>
+                            <h4>Chardonay</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                        </div>
+                        </div>
+                    </article>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <article class="">
+                        <div class="card">
+                        <img src="img/historia.png" class="card-img-top" alt="vino Historias" width="" height="">
+                        <div class="card-body">
+                            <h3>Card title</h3>
+                            <h4>Chardonay</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                        </div>
+                        </div>
+                    </article>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <article class="">
+                        <div class="card">
+                        <img src="img/historia.png" class="card-img-top" alt="vino Historias" width="" height="">
+                        <div class="card-body">
+                            <h3>Card title</h3>
+                            <h4>Chardonay</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                        </div>
+                        </div>
+                    </article>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <article class="">
+                        <div class="card">
+                        <img src="img/historia.png" class="card-img-top" alt="vino Historias" width="" height="">
+                        <div class="card-body">
+                            <h3>Card title</h3>
+                            <h4>Chardonay</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                        </div>
+                        </div>
+                    </article>
+            </div>
+
+    </div>
+    </div>
+
+  
+ 
+   
+    
+  </section>
+
+
+
+
+<?php include_once('include/footer.php') /*conecta con archibo footer*/?>
+
+    
