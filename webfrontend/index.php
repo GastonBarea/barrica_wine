914613ae978a819173c51b6775274e791070117d
Tba <sebastian.martinp6@gmail.com>
2021-04-25 20:10:30 -0300

    probando

--- webfrontend/index.php
+++ webfrontend/index.php
@@ -48,7 +48,7 @@
                         <div class="card">
                         <img src="https://fakeimg.pl/350x200/?text=World&font=lobster" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h3>Card title</h3>
+                            <h3>Carta</h3>
                             <h4>Chardonay</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="#" class="card-link"><span class="material-icons">favorite_border</span></a>
