119795edf9957bffa381b51f4d3c00d6b4e3c07e
GastonBarea <barea_gaston@gmail.com>
2021-04-29 20:02:46 -0300

    agregando bodegas json

---
+++ webfrontend/datos/d_productos.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$productos = array(
+    '1' => array(
+        'id' => 1,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '2' => array(
+        'id' => 1,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    )
+);
+
+
+
+
+
+
+
+?>
