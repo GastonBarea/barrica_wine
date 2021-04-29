d5de9c85e4b2fb9c0a7d9af1a522f331197b152d
GastonBarea <barea_gaston@gmail.com>
2021-04-28 22:06:54 -0300

    admin gaston

---
+++ webbackend/include/footer.php
@@ -0,0 +1,26 @@
+include_once('include/footer.php')
+
+<!-- Menú Footer -->
+  <footer class="main-footer">
+    <!-- A la derecha -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Por defecto a la izquierda -->
+    <p><strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>. </strong>All rights reserved.</p>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+
+
+<!-- SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
--- webbackend/starter.php
+++ webbackend/starter.php
@@ -146,66 +146,8 @@
   <!-- /.navbar -->
 
   <!-- Contenedor de la barra lateral principal -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Logotipo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Barrica Wine</span>
-    </a>
 
-    <!-- Barra lateral -->
-    <div class="sidebar">
-      <!-- Barra lateral panel de Usuario -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Gaston Barea</a>
-        </div>
-      </div>
-
-
-      <!-- Barra lateral menú -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!--Agregue iconos a los enlaces usando la clase .nav-icon con font-awesome o cualquier otra biblioteca de fuentes de iconos -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>Dasboard</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>Comentarios</p>
-            </a>
-          </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>Productos</p>
-            </a>
-          </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>Categorias</p>
-            </a>
-          </li>
-            <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>Marcas</p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.Barra lateral Menú -->
-    </div>
-    <!-- /.Barra lateral -->
-  </aside>
+  <?php  include_once('include/lateralvar.php') ?>
 
   <!-- Envoltorio de contenido. Contiene contenido de la página -->
   <div class="content-wrapper">
@@ -246,26 +188,4 @@
   <!-- /.control-sidebar -->
 
   <!-- Menú Footer -->
-  <footer class="main-footer">
-    <!-- A la derecha -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Por defecto a la izquierda -->
-    <p><strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>. </strong>All rights reserved.</p>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-
-
-<!-- SCRIPTS -->
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-</body>
-</html>
+ <?php  include_once('include/footer.php') ?>
