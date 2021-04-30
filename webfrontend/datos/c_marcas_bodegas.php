7c57de6543668bc28b79c78a08d9a5214095dcd2
GastonBarea <barea_gaston@gmail.com>
2021-04-29 23:07:58 -0300

    agregando marcas

--- webfrontend/datos/c_marcas_bodegas.php
+++ webfrontend/datos/c_marcas_bodegas.php
@@ -1,12 +1,14 @@
 <?php
 
 
-
-
-
-
-
-
+$data = file_get_contents("c_marcas_bodegas.json");
+$marcas = json_decode($data, true);
+ 
+foreach ($marcas as $marca) {
+    echo '<pre>';
+    print_r($marca);
+    echo '</pre>';
+}
 
 
 ?>
--- webfrontend/datos/d_productos.php
+++ webfrontend/datos/d_productos.php
@@ -14,7 +14,95 @@ $productos = array(
         'activa' => TRUE
     ),
     '2' => array(
-        'id' => 1,
+        'id' => 2,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '3' => array(
+        'id' => 3,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '4' => array(
+        'id' => 4,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '5' => array(
+        'id' => 5,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '6' => array(
+        'id' => 6,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '7' => array(
+        'id' => 7,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '8' => array(
+        'id' => 8,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '9' => array(
+        'id' => 9,
+        'nombre' => 'Vino Otoñal',
+        'precio' => '1500',
+        'cosecha' => '2011',
+        'categotiaTipo' => '',
+        'categotiaUva' => '',
+        'marca' => '',
+        'descripcion' => '',
+        'activa' => TRUE
+    ),
+    '10' => array(
+        'id' => 10,
         'nombre' => 'Vino Otoñal',
         'precio' => '1500',
         'cosecha' => '2011',
